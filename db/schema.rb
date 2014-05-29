806e1c725de384456c22c2c79b715eef48e59c5e
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2014-05-29 16:38:01 -0300

    Replace sqlite with pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140529181956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "twilio_credentials", force: true do |t|
     t.string   "sid"
     t.string   "auth_token"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20140529181956) do
     t.datetime "updated_at"
   end
 
-  add_index "twilio_credentials", ["phone_number"], name: "index_twilio_credentials_on_phone_number", unique: true
+  add_index "twilio_credentials", ["phone_number"], name: "index_twilio_credentials_on_phone_number", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -35,8 +38,8 @@ ActiveRecord::Schema.define(version: 20140529181956) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["phone_number"], name: "index_users_on_phone_number", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["phone_number"], name: "index_users_on_phone_number", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
